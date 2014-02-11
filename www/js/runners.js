39aad002f71ea6c74659a43280562b282e158a22
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-02-11 16:39:02 -0500

    work on runner storage

--- www/js/runners.js
+++ www/js/runners.js
@@ -3,17 +3,22 @@ var Runners = {
 
   init: function() {
     Runners.$form.addEventListener("submit", function(event) {
-    var entry = {
-      first_name: this.first_name.value,
-      last_name: this.last_name.value,
-      telephone: this.telephone.value
-    };
-  },
-  
+      var entry = {
+        first_name: this.first_name.value,
+        last_name: this.last_name.value,
+        telephone: this.telephone.value
+      };
+
+      Runners.add(entry);
+    },
+
   add: function(runner) {
     window.localStorage.setItem("Runners:" + runner.telephone, JSON.stringify(runner));
   },
 
+  get: function(telephone) {
+    window.localStoragegetItem("Runners:" + telephone);
+  },
 };
 Runners.init();
 
