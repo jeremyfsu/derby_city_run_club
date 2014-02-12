f71d42ee3cc5b2cb395e7c974aea812824d74c70
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-02-11 22:45:14 -0500

    progress on runner storage

--- www/js/runners.js
+++ www/js/runners.js
@@ -2,11 +2,13 @@ var Runners = {
   $form: document.getElementById("runners-form"),
 
   init: function() {
+    Runners.$form.reset();
     Runners.$form.addEventListener("submit", function(event) {
       var entry = {
+        run_count: parseInt(this.run_count.value),
         first_name: this.first_name.value,
         last_name: this.last_name.value,
-        telephone: this.telephone.value
+        telephone: this.telephone_number.value
       };
 
       Runners.add(entry);
@@ -18,7 +20,7 @@ var Runners = {
   },
 
   get: function(telephone) {
-    window.localStoragegetItem("Runners:" + telephone);
+    return window.localStorage.getItem("Runners:" + telephone);
   },
 };
 Runners.init();
