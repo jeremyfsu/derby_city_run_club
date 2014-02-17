4b1ffb7bda78162ea92823c76dab27b39d29a54e
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-02-17 17:19:07 -0500

    work on sign in function

--- www/js/sign_in.js
+++ www/js/sign_in.js
@@ -1,20 +1,22 @@
 var SignIn = {
-  init: function() {
-    runner = Runners.get(query_string('telephone');
-    if (runner != null) {
-      Runners.sign_in(runner);
-      /* Display message "you've run x times!" */
-    }
-    else {
-      /* Display error telephone not found, back button to home screen */
-    }
-  },
+  $result: document.getElementById("result"),
 
   query_string: function(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  },
+
+  init: function() {
+    runner = Runners.get(query_string('telephone'));
+    if (runner != null) {
+      Runners.sign_in(runner);
+      /* Display message "you've run x times!" */
+    }
+    else {
+     $result.innerHTML = 'Telephone number not found!'; 
+    }
   }
 
 };
