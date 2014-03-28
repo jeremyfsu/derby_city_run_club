955cba4fd6a3fd4c47d3de9e6ccd4cef0125f3a9
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-03-28 17:43:31 -0400

    sync work

--- www/js/sync.js
+++ www/js/sync.js
@@ -1,6 +1,15 @@
 var Sync = {
   init: function() {
     var blob = new Blob([Runners.toCSV()], {type: "text/csv"});
-    /* upload to google docs */
+    var client = new XMLHttpRequest();
+    var url = "https://www.googleapis.com/upload/drive/v2/files?uploadType=media";
+
+    client.open("PUT", url, false);
+    client.setRequestHeader("Content-Type", blob.type);
+    client.setRequestHeader("Content-Length", blob.size);
+    client.setRequestHeader("Authorization", auth);
+
+    client.send(blob);
+
   }
 };
